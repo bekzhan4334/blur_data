4dab22f2c649b112c8923c0bb459038662bd2899
bekzhan43 <bekzha4334@gmail.com>
2021-12-07 06:37:27 +0600

    regex for name

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cv2
 import pytesseract
+import re
+
+#patterns
+name_pattern = '^[A-Z][-a-zA-Z]+$'
 
 # path to tesseract.exe
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
@@ -11,6 +15,7 @@ img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 config = r'--oem 3 --psm 6'
 
 data = pytesseract.image_to_data(img, config=config)
+blur_img = img.copy()
 
 for i, el in enumerate(data.splitlines()):
     if i == 0:
@@ -18,17 +23,16 @@ for i, el in enumerate(data.splitlines()):
     el = el.split()
     try:
         x, y, w, h = int(el[6]), int(el[7]), int(el[8]), int(el[9])
-        if el[11]=='ANDREI':
-            cv2.rectangle(img, (x, y), (w+x, h+y), (0, 0, 255), 1)
+        if re.match(name_pattern, el[11]):
             # Grab ROI with Numpy slicing and blur
-
             ROI = img[y:y + h, x:x + w]
             blur = cv2.GaussianBlur(ROI, (23, 23), 0)
-            img[y:y + h, x:x + w] = blur
+            blur_img[y:y + h, x:x + w] = blur
     except IndexError:
         print('No data in the image')
 # Insert ROI back into image
 
 # show photo
-cv2.imshow('Result',img)
+cv2.imshow('Before',img)
+cv2.imshow('After',blur_img)
 cv2.waitKey(0)
