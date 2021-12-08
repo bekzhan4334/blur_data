e90a2102b14ecbc22f2020df74d93e8361fd0706
bekzhan43 <bekzha4334@gmail.com>
2021-12-08 11:44:27 +0600

    regex for date

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import re
 #patterns
 name_pattern = '^[A-Z][-a-zA-Z]+$'
 passport_pattern = '^(?!^0+$)[a-zA-Z0-9]{3,20}$'
+date_pattern = r'(?<!\d)(?:0?[1-9]|[12][0-9]|3[01])-(?:0?[1-9]|1[0-2])-(?:19[0-9][0-9]|20[01][0-9])(?!\d)'
 
 # path to tesseract.exe
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 # location of the image
-img = cv2.imread('D:\\TG_bots\\data_finder\\images\\ticket.jpg')
+img = cv2.imread('D:\\TG_bots\\data_finder\\images\\example.jpg.jpg')
 # converting to RGB
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 # config for better quality of detecting
@@ -24,7 +25,7 @@ for i, el in enumerate(data.splitlines()):
     el = el.split()
     try:
         x, y, w, h = int(el[6]), int(el[7]), int(el[8]), int(el[9])
-        if re.match(name_pattern, el[11]) or re.match(passport_pattern, el[11]):
+        if re.match(name_pattern, el[11]) or re.match(passport_pattern, el[11]) or re.match(date_pattern, el[11]):
             #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)  => green rectangle
             # Grab ROI with Numpy slicing and blur
             ROI = img[y:y + h, x:x + w]
